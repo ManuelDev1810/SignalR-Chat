153ea16451ccbf44ebab2704b06c5d4d1bc09f4e
Manuel De Jesus Guerrero Vasquez <manueldejesusguerrerovasquez@Manuels-MacBook-Pro.local>
2020-12-19 20:14:21 -0400

    Setting up the StockBoat start

--- JobSityChat.BE/JobSityChat.StockBot/Program.cs
+++ JobSityChat.BE/JobSityChat.StockBot/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Threading;
+using JobSityChat.Infrastructure.Services.Handlers;
+using JobSityChat.StockBot.MBQueues;
 
 namespace JobSityChat.StockBot
 {
     class Program
     {
+        private static readonly AutoResetEvent _closing = new AutoResetEvent(false);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Setting up the producer
+            StockResponseQueueProducer producer = new ();
+            StockHandler stockHandler = new();
+
+            //Setting up the consumer
+            StockRequestQueueConsumer consumer = new (stockHandler, producer);
+
+            //Start to listen for the Api
+            consumer.Run();
+
+            Console.WriteLine("Waiting for request to look for stocks");
+
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(OnExit);
+
+            _closing.WaitOne();
+        }
+
+        private static void OnExit(object sender, ConsoleCancelEventArgs e)
+        {
+            Console.WriteLine("Exit");
+            _closing.Set();
         }
     }
 }
